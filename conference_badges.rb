a80946462640229a9c42289c8ffa2d0b9fbecdb8
Shaqqour M <mshaqqourm@gmail.com>
2019-09-13 19:24:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,8 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
-  array.map { |name| badge_maker(name) }
+def batch_badge_creator(names)
+  names.map { |name| badge_maker(name) }
 end
+
+def assign_rooms(speakers)
