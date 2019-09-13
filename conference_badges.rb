b2ed7207b73be76d41527b112c2a2e7b63a6e930
Shaqqour M <mshaqqourm@gmail.com>
2019-09-13 19:40:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,7 +12,7 @@ def assign_rooms(speakers)
   end
 end
 
-def printer(att)
-  puts batch_badge_creator
-  puts assign_rooms
+def printer(attendees)
+  puts batch_badge_creator(attendees)
+  puts assign_rooms(attendees)
 end
